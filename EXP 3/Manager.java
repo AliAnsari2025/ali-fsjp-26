2b4c2f289c574a9de422653f7638d5f8fb8e5e38
AliAnsari2025 <mdaliansari@eng.rizvi.edu.in>
2026-09-23 23:28:55 +0530

    Create Manager.java

---
+++ EXP 3/Manager.java
@@ -0,0 +1,20 @@
+// Aim: Programs on various types of inheritance and Exception handling.
+// Name: Ansari Mohd Ali 
+// Roll no./UIN: 02/251P011
+// Class: S.E.Computer A
+public class Manager extends Employee{
+    private int number_of_projects;
+    private int number_of_team_members;
+
+    Manager(String name, int age, String gender, int salary, String working_hours, int employee_id, String department, int number_of_projects, int number_of_team_members){
+        super(name,age, gender, salary, working_hours, employee_id, department);
+        this.number_of_projects = number_of_projects;
+        this.number_of_team_members = number_of_team_members;
+    }
+
+void display(){
+        super.display();
+        System.out.println("Number of Projects Completed: " + this.number_of_projects);
+        System.out.println("Number of Members in Team: " + this.number_of_team_members);
+    }
+}
